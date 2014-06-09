0fcbb5b97eab995f53bbc977b8f07a3f6470d4a2
MaxGfeller <max.gfeller@gmail.com>
2014-06-09 23:18:03 +0200

    Render question answer options

--- js/components/question-display.js
+++ js/components/question-display.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var inherits = require('util').inherits;
 var Widget = require('browserify-widget');
+var domify = require('domify');
 
 inherits(QuestionDisplay, Widget);
 
@@ -14,6 +15,11 @@ QuestionDisplay.prototype.setQuestion = function(question) {
     var el = document.querySelector('#question');
     el.setAttribute('data-id', question.id);
     el.textContent = question.text;
+
+    var aEl = document.querySelector('.answers');
+    question.answers.forEach(function(answer, i) {
+        aEl.appendChild(domify('<button class="btn btn-block btn-outlined" id="answer-' + (i + 1) + '">' + answer + '</button>'))
+    });
 }
 
 module.exports = QuestionDisplay;
