82885c1c08e807a1b5bc6c55ed0ba9ba3ee7ab19
MaxGfeller <max.gfeller@gmail.com>
2014-06-11 22:43:35 +0200

    Add functionality for buttons

--- js/components/question-display.js
+++ js/components/question-display.js
@@ -6,20 +6,37 @@ var domify = require('domify');
 inherits(QuestionDisplay, Widget);
 
 var html = fs.readFileSync(__dirname + '/question-display.html', 'utf8');
+var buttonHtml = fs.readFileSync(__dirname + '/question-button.html', 'utf8');
 
 function QuestionDisplay() {
     Widget.call(this, html);
 }
 
-QuestionDisplay.prototype.setQuestion = function(question) {
+QuestionDisplay.prototype.setQuestion = function(question, onclick) {
     var el = document.querySelector('#question');
     el.setAttribute('data-id', question.id);
     el.textContent = question.text;
 
     var aEl = document.querySelector('.answers');
     question.answers.forEach(function(answer, i) {
-        aEl.appendChild(domify('<button class="btn btn-block btn-outlined" id="answer-' + (i + 1) + '">' + answer + '</button>'))
+        var btn = domify(buttonHtml);
+        btn.id = 'answer-' + (i + 1);
+        btn.textContent = answer;
+        btn.onclick = onclick;
+        aEl.appendChild(btn);
     });
+}
+
+QuestionDisplay.prototype.hide = function() {
+    this.renderedTo.forEach(function(el) {
+        el.querySelector('.question-wrapper').style.display = 'none';
+    });
+}
+
+QuestionDisplay.prototype.show = function() {
+    this.renderedTo.forEach(function(el) {
+        el.querySelector('.question-wrapper').style.display = 'block';
+    })
 }
 
 module.exports = QuestionDisplay;
