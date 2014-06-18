726b86b9316aed5c5edf4073052743ba87468c07
MaxGfeller <max.gfeller@gmail.com>
2014-06-18 14:13:56 +0200

    Don't show absolute numbers for each answer. Fixes #4

--- js/components/question-details.js
+++ js/components/question-details.js
@@ -22,9 +22,6 @@ module.exports = function(el, question) {
         answer.percentage = (countTotal > 0) ? (100/countTotal) * parseInt(answer.count) : 0;
         var div = document.createElement('div');
         div.className = 'result result-' + i;
-        var p = document.createElement('p');
-        p.textContent = answer.count;
-        div.appendChild(p);
         var p2 = document.createElement('p');
         p2.className = 'number';
         p2.textContent = Math.floor(answer.percentage) + '%';
